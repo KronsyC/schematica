0433e45c9d79beae2d3a77f2bc26eb76bc31c6d0
KronsyC <87078913+KronsyC@users.noreply.github.com>
2022-02-11 18:59:07 +0000

    Fixed to make default import work in JS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 
-export * from "./lib/Schemas"
 import Validator from "./lib/utilities/Validator/Validator"
 import Schematica from "./Schematica"
 import Parser from "./lib/utilities/Parser/Parser"
 import Encoder from "./lib/utilities/Encoder/Encoder"
 import Presets from "./lib/Schemas"
+
+module.exports = exports = Schematica
+Object.defineProperty(exports, "__esModule", {value: true})
+
+export * from "./lib/Schemas"
 export {
     Validator,
     Parser,
